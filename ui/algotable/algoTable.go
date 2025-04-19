cd70b41da2b8e8b153ce97dcc9de1e6f6b2a90e4
Paula Joyce Ucol <peaksenut@gmail.com>
2025-04-19 11:16:37 +0800

    docs: comments

--- ui/algotable/algoTable.go
+++ ui/algotable/algoTable.go
@@ -8,53 +8,62 @@ import (
 	"github.com/rivo/tview"
 )
 
-	var Table = tview.NewTable().
-		SetBorders(true)
-	var tableHeaders = strings.Split("Step_Page_Frames Content_Page Fault?", "_")
+// table to show the process of the selected algorithms
 
-	func PopulateTable(prsRange int, prs []int, frames int) {
-		var cols = 4 // steps, page, frame contents, page fault?
-		var rows = prsRange + 1
-		Table.Clear()
+var Table = tview.NewTable().SetBorders(true)
+var tableHeaders = strings.Split("Step_Page_Frames Content_Page Fault?", "_")
 
-		for c := 0; c < cols; c++ {
-			Table.SetCell(0, c,
-				tview.NewTableCell(tableHeaders[c]).
-					SetTextColor(tcell.ColorOrange).
-					SetAlign(tview.AlignCenter))
-		}
+// this function puts the contents inside the table
+func PopulateTable(prsRange int, prs []int, frames int) {
+	var cols = 4 // steps, page, frame contents, page fault?
+	var rows = prsRange + 1 // number of rows depends on the range selected + 1, because step 0 exists
+	
+	// clear the table every time frames and prs range changes to
+	// remove previous data 
+	Table.Clear()
 
-		for steps := 0; steps < rows; steps++ {
-			Table.SetCell(steps +1, 0,
-				tview.NewTableCell(fmt.Sprintf("%d", steps)).
-					SetTextColor(tcell.ColorWhite).
-					SetAlign(tview.AlignCenter))
-		}
+	// table headers
+	for c := 0; c < cols; c++ {
+		Table.SetCell(0, c,
+			tview.NewTableCell(tableHeaders[c]).
+				SetTextColor(tcell.ColorOrange).
+				SetAlign(tview.AlignCenter))
+	}
 
-		// page column
-		for i := 0; i < prsRange; i++ {
-			Table.SetCell(i + 2, 1,
-				tview.NewTableCell(fmt.Sprintf("%d", prs[i])).
-					SetTextColor(tcell.ColorOrange).
-					SetAlign(tview.AlignCenter))
-		}
-
-		// TODO: frame column
-
-		Table.SetCell(1, 2,
-			tview.NewTableCell(strings.Repeat("[ ]", frames)).
+	// steps --- starts from 0
+	for steps := 0; steps < rows; steps++ {
+		Table.SetCell(steps +1, 0,
+			tview.NewTableCell(fmt.Sprintf("%d", steps)).
 				SetTextColor(tcell.ColorWhite).
 				SetAlign(tview.AlignCenter))
-
-		Table.Select(0, 0).SetFixed(1, 1).SetDoneFunc(func(key tcell.Key) {
-			if key == tcell.KeyEnter {
-				Table.SetSelectable(true, true)
-			}
-		}).SetSelectedFunc(func(row int, column int) {
-			Table.GetCell(row, column).SetTextColor(tcell.ColorRed)
-			Table.SetSelectable(false, false)
-		})
 	}
+
+	// page column
+	for i := 0; i < prsRange; i++ {
+		Table.SetCell(i + 2, 1,
+			tview.NewTableCell(fmt.Sprintf("%d", prs[i])).
+				SetTextColor(tcell.ColorOrange).
+				SetAlign(tview.AlignCenter))
+	}
+
+	// TODO: frame column
+
+	// put empty frames at step 0
+	Table.SetCell(1, 2,
+		tview.NewTableCell(strings.Repeat("[ ]", frames)).
+			SetTextColor(tcell.ColorWhite).
+			SetAlign(tview.AlignCenter))
+
+	// added cool navigation stuff i found on the internet
+	Table.Select(0, 0).SetFixed(1, 1).SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEnter {
+			Table.SetSelectable(true, true)
+		}
+	}).SetSelectedFunc(func(row int, column int) {
+		Table.GetCell(row, column).SetTextColor(tcell.ColorRed)
+		Table.SetSelectable(false, false)
+	})
+}
 
 	
 
